246aee90a5c73ab06d83acd9352facef87a6d45b
john-g1t <skorniakovschool@gmail.com>
2026-02-05 12:13:33 +0300

    deploy

--- src/composables/use-is-desktop.ts
+++
@@ -1,8 +0,0 @@
-import { useWindowSize } from '@vueuse/core'
-import { computed } from 'vue'
-
-const { width } = useWindowSize()
-
-export function useIsDesktop(desktopSize = 1000) {
-  return computed(() => width.value > desktopSize)
-}
