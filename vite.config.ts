5d972e51fb2297989eaca5de7fcd6e4a559bf11f
john-g1t <skorniakovschool@gmail.com>
2026-02-05 12:35:22 +0300

    path

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { defineConfig } from 'vite'
 import svgLoader from 'vite-svg-loader'
 
 export default defineConfig({
+  base: '/neuromionics-landing/',
   plugins: [
     vue(),
     svgLoader(),
